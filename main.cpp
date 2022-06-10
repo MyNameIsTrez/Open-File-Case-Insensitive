f573129be33ea29390f3dae94cc2d3977ff1ff96
MyNameIsTrez <welfje@gmail.com>
2022-06-10 02:27:21 +0200

    Add tests.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <filesystem>
 #include <string>
+#include <cstring>
 #include <cassert>
 
 class System
@@ -33,7 +34,7 @@ FILE *OpenFileCaseInsensitive(const std::filesystem::path filePath, const std::s
 
 		for (const std::filesystem::path &filesystemEntryPath : std::filesystem::directory_iterator{inspectedPath})
 		{
-			if (StringsEqualCaseInsensitive(filesystemEntryPath.filename().string(), (*filePathIterator).string()))
+			if (StringsEqualCaseInsensitive(filesystemEntryPath.filename(), (*filePathIterator)))
 			{
 				inspectedPath = filesystemEntryPath;
 				pathPartExists = true;
@@ -46,7 +47,7 @@ FILE *OpenFileCaseInsensitive(const std::filesystem::path filePath, const std::s
 			// If this is the last part, then all directories in filePath exist, but the file doesn't.
 			if (std::next(filePathIterator) == filePath.end())
 			{
-				return fopen((inspectedPath / filePath.filename()).string().c_str(), mode.c_str());
+				return fopen((inspectedPath / filePath.filename()).c_str(), mode.c_str());
 			}
 
 			// Some directory in filePath doesn't exist, so the file can't be created.
@@ -55,32 +56,67 @@ FILE *OpenFileCaseInsensitive(const std::filesystem::path filePath, const std::s
 	}
 
 	// If the file exists, open it.
-	return fopen(inspectedPath.string().c_str(), mode.c_str());
+	return fopen(inspectedPath.c_str(), mode.c_str());
 }
 
-void TestCaseInsensitivity()
+void TestFileCaseInsensitivityCreationReadingWritingAppending(void)
 {
-	FILE *a = OpenFileCaseInsensitive("f1", "w");
-	fwrite("foo", 1, 3, a);
-	fclose(a);
+	FILE *filePointer1 = OpenFileCaseInsensitive("f1", "w");
+	fwrite("foo", 1, 3, filePointer1);
+	fclose(filePointer1);
 
 	char buffer[42];
-	FILE *b = OpenFileCaseInsensitive("F1", "r");
-	fgets(buffer, 42, b);
-	assert(buffer == "foo");
+	memset(buffer, 0, 42);
+
+	// a+ means 1. read and write 2. without truncating 3. starting at the end of the file.
+	FILE *filePointer2 = OpenFileCaseInsensitive("F1", "a+");
+	fwrite("bar", 1, 3, filePointer2);
+
+	fseek(filePointer2, 0, SEEK_SET);
+	std::string result = fgets(buffer, 42, filePointer2);
+
+	std::string expected = "foobar";
+	assert(result == expected);
 }
 
-int main()
+void TestDirectoryDoesntExist(void)
 {
-	std::filesystem::path cc_game_path{System::GetWorkingDirectory()};
-	std::filesystem::create_directories(cc_game_path);
+	FILE *filePointer = OpenFileCaseInsensitive(std::filesystem::path("a1") / "f1", "w");
+	assert(filePointer == nullptr);
+}
 
-	TestCaseInsensitivity();
+void TestDirectoryCaseInsensitivity(void)
+{
+	std::filesystem::create_directories(std::filesystem::path(System::GetWorkingDirectory()) / "a1");
 
-	// std::filesystem::create_directories(cc_game_path / "a1" / "b1");
-	// std::filesystem::create_directories(cc_game_path / "a2");
+	FILE *filePointer1 = OpenFileCaseInsensitive(std::filesystem::path("a1") / "f1", "w");
+	fwrite("foo", 1, 3, filePointer1);
+	fclose(filePointer1);
 
-	std::filesystem::remove_all(cc_game_path);
+	char buffer[42];
+	memset(buffer, 0, 42);
+
+	FILE *filePointer2 = OpenFileCaseInsensitive(std::filesystem::path("A1") / "f1", "a+");
+	fwrite("bar", 1, 3, filePointer2);
+
+	fseek(filePointer2, 0, SEEK_SET);
+	std::string result = fgets(buffer, 42, filePointer2);
+
+	std::string expected = "foobar";
+	assert(result == expected);
+}
+
+int main(void)
+{
+	std::filesystem::remove_all(System::GetWorkingDirectory());
+
+	std::filesystem::create_directories(System::GetWorkingDirectory());
+
+	TestFileCaseInsensitivityCreationReadingWritingAppending();
+	TestDirectoryDoesntExist();
+	TestDirectoryCaseInsensitivity();
+
+	std::filesystem::remove_all(System::GetWorkingDirectory());
 
 	return (0);
 }
