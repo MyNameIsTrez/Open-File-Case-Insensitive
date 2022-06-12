575924ea8bcdfaa22780bf8b000b7821f7b49651
MyNameIsTrez <welfje@gmail.com>
2022-06-12 20:19:53 +0200

    Fix relative pathing issues.

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ private:
 	static std::string s_WorkingDirectory;
 };
 
-std::string System::s_WorkingDirectory = "cccp";
+std::string System::s_WorkingDirectory = "I:/foo/bar/cccp";
 
 bool StringsEqualCaseInsensitive(const std::string &a, const std::string &b)
 {
@@ -28,13 +28,16 @@ FILE *OpenFileCaseInsensitive(const std::filesystem::path filePath, const std::s
 {
 	std::filesystem::path inspectedPath{System::GetWorkingDirectory()};
 
-	for (std::filesystem::path::const_iterator filePathIterator = filePath.begin(); filePathIterator != filePath.end(); ++filePathIterator)
+	const std::filesystem::path relativeFilePath = filePath.lexically_relative(inspectedPath);
+
+	for (std::filesystem::path::const_iterator relativeFilePathIterator = relativeFilePath.begin(); relativeFilePathIterator != relativeFilePath.end(); ++relativeFilePathIterator)
 	{
 		bool pathPartExists = false;
 
+		// Check if a path part (directory or file) exists in the filesystem.
 		for (const std::filesystem::path &filesystemEntryPath : std::filesystem::directory_iterator{inspectedPath})
 		{
-			if (StringsEqualCaseInsensitive(filesystemEntryPath.filename(), (*filePathIterator)))
+			if (StringsEqualCaseInsensitive(filesystemEntryPath.filename().string(), (*relativeFilePathIterator).string()))
 			{
 				inspectedPath = filesystemEntryPath;
 				pathPartExists = true;
@@ -44,24 +47,24 @@ FILE *OpenFileCaseInsensitive(const std::filesystem::path filePath, const std::s
 
 		if (!pathPartExists)
 		{
-			// If this is the last part, then all directories in filePath exist, but the file doesn't.
-			if (std::next(filePathIterator) == filePath.end())
+			// If this is the last part, then all directories in relativeFilePath exist, but the file doesn't.
+			if (std::next(relativeFilePathIterator) == relativeFilePath.end())
 			{
-				return fopen((inspectedPath / filePath.filename()).c_str(), mode.c_str());
+				return fopen((inspectedPath / relativeFilePath.filename()).string().c_str(), mode.c_str());
 			}
 
-			// Some directory in filePath doesn't exist, so the file can't be created.
+			// Some directory in relativeFilePath doesn't exist, so the file can't be created.
 			return nullptr;
 		}
 	}
 
 	// If the file exists, open it.
-	return fopen(inspectedPath.c_str(), mode.c_str());
+	return fopen(inspectedPath.string().c_str(), mode.c_str());
 }
 
 void TestFileCaseInsensitivityCreationReadingWritingAppending(void)
 {
-	FILE *filePointer1 = OpenFileCaseInsensitive("f1", "w");
+	FILE *filePointer1 = OpenFileCaseInsensitive(std::filesystem::path(System::GetWorkingDirectory()) / "f1", "w");
 	fwrite("foo", 1, 3, filePointer1);
 	fclose(filePointer1);
 
@@ -69,7 +72,7 @@ void TestFileCaseInsensitivityCreationReadingWritingAppending(void)
 	memset(buffer, 0, 42);
 
 	// a+ means 1. read and write 2. without truncating 3. starting at the end of the file.
-	FILE *filePointer2 = OpenFileCaseInsensitive("F1", "a+");
+	FILE *filePointer2 = OpenFileCaseInsensitive(std::filesystem::path(System::GetWorkingDirectory()) / "F1", "a+");
 	fwrite("bar", 1, 3, filePointer2);
 
 	fseek(filePointer2, 0, SEEK_SET);
@@ -81,7 +84,7 @@ void TestFileCaseInsensitivityCreationReadingWritingAppending(void)
 
 void TestDirectoryDoesntExist(void)
 {
-	FILE *filePointer = OpenFileCaseInsensitive(std::filesystem::path("a1") / "f1", "w");
+	FILE *filePointer = OpenFileCaseInsensitive(std::filesystem::path(System::GetWorkingDirectory()) / "a1" / "f1", "w");
 	assert(filePointer == nullptr);
 }
 
@@ -89,14 +92,14 @@ void TestDirectoryCaseInsensitivity(void)
 {
 	std::filesystem::create_directories(std::filesystem::path(System::GetWorkingDirectory()) / "a1");
 
-	FILE *filePointer1 = OpenFileCaseInsensitive(std::filesystem::path("a1") / "f1", "w");
+	FILE *filePointer1 = OpenFileCaseInsensitive(std::filesystem::path(System::GetWorkingDirectory()) / "a1" / "f1", "w");
 	fwrite("foo", 1, 3, filePointer1);
 	fclose(filePointer1);
 
 	char buffer[42];
 	memset(buffer, 0, 42);
 
-	FILE *filePointer2 = OpenFileCaseInsensitive(std::filesystem::path("A1") / "f1", "a+");
+	FILE *filePointer2 = OpenFileCaseInsensitive(std::filesystem::path(System::GetWorkingDirectory()) / "A1" / "f1", "a+");
 	fwrite("bar", 1, 3, filePointer2);
 
 	fseek(filePointer2, 0, SEEK_SET);
